f7dd0d8f6f3b67f68b238337583716046a455026
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-10 15:15:01 -0500

    Added data storage module

---
+++ tweetBank.js
@@ -0,0 +1,45 @@
+var _ = require('underscore');
+
+// global data array
+var data = [];
+
+// add an element to our data array
+function add ( name, text ) {
+  data.push( { name: name, text: text } );
+}
+
+// make a shallow copy of the data array
+function list () {
+  return _.clone( data );
+}
+
+// find an element in the data array
+function find ( properties ) {
+  return _.where( data, properties );
+}
+
+module.exports = {
+  add: add,
+  list: list,
+  find: find
+};
+
+var randArrayEl = function( arr ) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getFakeName = function() {
+  var fakeFirsts = ['Nimit', 'Dave', 'Will', 'Charlotte', 'Jacob','Ethan','Sophia','Emma','Madison'];
+  var fakeLasts = ["Alley", 'Stacky', 'Fullstackerson', 'Nerd', 'Ashby', 'Gatsby', 'Hazelnut', 'Cookie', 'Tilde', 'Dash'];
+    return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+var getFakeTweet = function() {
+  var awesome_adj = ['awesome','breathtaking','amazing','sexy','sweet','cool','wonderful','mindblowing'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for(var i=0; i<10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
