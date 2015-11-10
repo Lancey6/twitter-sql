32568874d82c31802ed190b7679349a525af2236
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-10 15:02:45 -0500

    Set up swig as our rendering engine

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express'); 
+var swig = require('swig');
+
 var app = express(); 
 
+// app configuration
+app.engine( 'html', swig.renderFile );        // render html using swig.renderFile
+app.set( 'view engine', 'html' );             // use html as default view engine
+app.set( 'views', process.cwd() + '/views' ); // render templates from our views folder
+
+swig.setDefaults( { cache: false } );         // disable swig caching
+
+// start app
 app.listen(3000, function() {
 	console.log('Listening on port 3000'); 
 })
-
 
 //Log incoming requests
 app.use('/', function(req, res, next) {
@@ -16,7 +25,10 @@ app.use('/', function(req, res, next) {
 	next(); 
 })
 
+// root route /
 app.get('/', function(req, res) {
-	res.send('test'); 
+  var people = [{name: 'Full'}, {name: 'Stacker'}];
+
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
