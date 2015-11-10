6a35940e0737abdfe6b06029417b683f45405493
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-10 15:42:41 -0500

    Added public route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var tweetBank = require('../tweetBank');
 
 router.get('/', function( req, res, next ) {
   var tweets = tweetBank.list();
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+} );
+
+router.use('/', function( req, res, next ) {
+  fs.readFile( process.cwd() + '/public' + req.path, 'utf8', function( err, data ) {
+    if ( err ) {
+      res.status(404).send( { error: 'File not found' } );
+      return console.error( err );
+    }
+
+    res.send( data );
+  } );
 } );
 
 module.exports = router;
